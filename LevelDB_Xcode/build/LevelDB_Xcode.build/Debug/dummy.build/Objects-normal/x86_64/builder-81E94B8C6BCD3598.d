2ef2509ef59faf613dc9a26b51fa226c03680230
dencesun <sunxiaowei199325@163.com>
2017-12-03 19:58:25 +0800

    first commit

---
+++ LevelDB_Xcode/build/LevelDB_Xcode.build/Debug/dummy.build/Objects-normal/x86_64/builder-81E94B8C6BCD3598.d
@@ -0,0 +1,10 @@
+dependencies: /Users/dencesun/Desktop/learn_levelDB/db/builder.cc \
+  ../db/builder.h ../include/leveldb/status.h \
+  ../include/leveldb/export.h ../include/leveldb/slice.h \
+  ../db/filename.h ../port/port.h ../db/dbformat.h \
+  ../include/leveldb/comparator.h ../include/leveldb/db.h \
+  ../include/leveldb/iterator.h ../include/leveldb/options.h \
+  ../include/leveldb/filter_policy.h ../include/leveldb/table_builder.h \
+  ../util/coding.h ../util/logging.h ../db/table_cache.h \
+  ../include/leveldb/cache.h ../include/leveldb/table.h \
+  ../db/version_edit.h ../include/leveldb/env.h
---
+++ LevelDB_Xcode/dummy/main.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  leveldb
+//
+//  Created by dencesun on 01/12/2017.
+//  Copyright © 2017 Sun. All rights reserved.
+//
+
+#include <iostream>
+#include "leveldb/db.h"
+
+using namespace std;
+using namespace leveldb;
+
+int main() {
+    DB *db ;
+    Options op;
+    op.create_if_missing = true;
+    Status s = DB::Open(op,"/tmp/testdb",&db);
+
+    if(s.ok()){
+        cout << "创建成功" << endl;
+        s = db->Put(WriteOptions(),"abcd","1234");
+        if(s.ok()){
+            cout << "插入数据成功" << endl;
+            string value;
+            s = db->Get(ReadOptions(),"abcd",&value);
+            if(s.ok()){
+                cout << "获取数据成功,value:" << value << endl;
+            }
+            else{
+                cout << "获取数据失败" << endl;
+            }
+        }
+        else{
+            cout << "插入数据失败" << endl;
+        }
+    }
+    else{
+        cout << "创建数据库失败" << endl;
+    }
+    delete db;
+    return 0;
+}
+
